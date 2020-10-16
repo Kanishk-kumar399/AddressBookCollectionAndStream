10deee996c48c605ca72389224f152c001349e4f
Kanishk-kumar399 <kanishkk301198@gmail.com>
2020-10-16 15:11:45 +0530

    [Kumar Kanishk] added UC5:new feature for adding multiple contacts using collections

--- com/Addressbook/AddressBookMain.java
+++ com/Addressbook/AddressBookMain.java
@@ -3,54 +3,51 @@ import java.util.*;
 public class AddressBookMain 
 {
 	static Scanner sc=new Scanner(System.in);
-	int numOfContact=0;
-	private Contact[] contactArray;
+	private ArrayList<Contact> contactArrayList;
 	
 	public AddressBookMain()
 	{
-		contactArray=new Contact[5];
+		contactArrayList=new ArrayList<>();
 	}
 	private void addNewContact(String firstName,String lastName,String address,String city,String state,int pinCode,long mobiileNumber,String emailId)
 	{
-		contactArray[numOfContact]=new Contact(firstName,lastName,address,city,state,pinCode,mobiileNumber,emailId);
-		System.out.println(contactArray[numOfContact]);
-		numOfContact++;
-	}
-	public void printContact()
-	{
-	System.out.println(contactArray);
+		Contact object=new Contact(firstName,lastName,address,city,state,pinCode,mobiileNumber,emailId);
+		contactArrayList.add(object);
 	}
 	public void editContact()
 	{
 		System.out.println("Enter The First Name And Last Name to edit the contact details");
 		String firstName=sc.next();
 		String lastName=sc.next();
-		for(int i = 0; i < numOfContact; i++) 
+		for(int i = 0; i <contactArrayList.size() ; i++) 
 		{
-			if(contactArray[i].firstName.equals(firstName) && contactArray[i].lastName.equals(lastName)) 
+			Contact object=contactArrayList.get(i);
+			if(object.firstName.equals(firstName) && object.lastName.equals(lastName)) 
 			{
 				System.out.print("Enter address,city,state,pincode,phonenumber,email");
 				String ad=sc.next();
-				contactArray[i].setAddress(ad);
+				object.setAddress(ad);
 				String ci=sc.next();
-				contactArray[i].setCity(ci);
+				object.setCity(ci);
 				String st=sc.next();
-				contactArray[i].setState(st);
+				object.setState(st);
 				int pin=sc.nextInt();
-				contactArray[i].setPinCode(pin);
+				object.setPinCode(pin);
 				long num=sc.nextLong();
-				contactArray[i].setPhoneNumber(num);
+				object.setPhoneNumber(num);
 				String em=sc.next();
-				contactArray[i].setEmailId(em);
-				System.out.println(contactArray[i]);
+				object.setEmailId(em);
+				System.out.println(contactArrayList);
 				break;
 			}
 		}
 	}
 	public void deleteContactDetails(String firstName, String lastName) {
-		for(int i = 0; i < numOfContact; i++) {
-			if(contactArray[i].firstName.equals(firstName) && contactArray[i].lastName.equals(lastName)) {
-				contactArray[i] = null;
+		for(int i = 0; i < contactArrayList.size(); i++) 
+		{
+			Contact object=contactArrayList.get(i);
+			if(object.firstName.equals(firstName) && object.lastName.equals(lastName)) {
+				contactArrayList.remove(i);
 				System.out.println("Contact deleted");
 				break;
 			}
