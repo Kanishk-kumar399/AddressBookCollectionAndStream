78f4536f86e852c66afa55694faab834b6b1dab2
Kanishk-kumar399 <kanishkk301198@gmail.com>
2020-10-16 19:24:19 +0530

    [Kumar Kanishk] added UC9:new feature to view by state or city

--- com/Addressbook/AddressBookManagement.java
+++ com/Addressbook/AddressBookManagement.java
@@ -5,6 +5,8 @@ public class AddressBookManagement
 {
 	static Scanner sc=new Scanner(System.in);
 	public static Map<String,AddressBookMain> nameToAddressBookMap=new TreeMap<>();
+	public static Map<String, Contact> contactCityMap = new TreeMap<>();
+	public static Map<String, Contact> contactStateMap = new TreeMap<>();
 	public static void addNewAddressBook()
 	{
 		AddressBookMain object=new AddressBookMain();
@@ -12,9 +14,6 @@ public class AddressBookManagement
 		String name=sc.next();
 		object.maintainAddressBook();
 		nameToAddressBookMap.put(name,object);
-	}
-	public enum SearchBy {
-		CITY, STATE
 	}
 	/*UC8 search person by city/state in all address books*/
 	public static void searchPersonInBook(String search)
@@ -38,15 +37,66 @@ public class AddressBookManagement
 				System.out.println("No person was found");
 		}
 	}
+	private static void showContactGroupedByCity() {
+		Set<String> listOfCity = contactCityMap.keySet();
+		for(String cityName : listOfCity) {
+			System.out.println("Contact Entries for CITY: " + cityName);
+			searchPersonInBook(cityName);
+		}
+	}
+
+	private static void showContactGroupedByState() {
+		Set<String> listOfState = contactStateMap.keySet();
+		for(String stateName : listOfState) {
+			System.out.println("Contact Entries for STATE: " + stateName);
+			searchPersonInBook(stateName);
+		}
+	}
 	public static void main(String args[])
 	{
 		AddressBookManagement aBookManager=new AddressBookManagement();
-		System.out.println("Enter 1 to add a new BOOK");
-		int choice=sc.nextInt();
-		if(choice==1)
-			aBookManager.addNewAddressBook();
-		else
-			System.out.println("Invalid choice");
-		System.out.println(aBookManager.nameToAddressBookMap);
+		while(true)
+		{
+			System.out.println("\n1. Add a new Address Book");
+			System.out.println("\n2. Search person across all address books");
+			System.out.println("\n3. Show contact  by city");
+			System.out.println("\n4. Show contacts by state");
+			System.out.println("\n5. Exit");
+			System.out.println("\n Please Enter your choice");
+			int choice=sc.nextInt();
+			switch(choice)
+			{
+			case 1:
+			while (true) {
+				AddressBookMain m = new AddressBookMain();
+				System.out.println("Enter A name for address Book");
+				String name = sc.next();
+				if (aBookManager.nameToAddressBookMap.containsKey(name)) {
+					System.out.println("\nAddress Book already exists !!!\n");
+					continue;
+				} else {
+					aBookManager.nameToAddressBookMap.put(name, m);
+					System.out.println("\n In Address Book : " + name);
+					m.maintainAddressBook();
+					break;
+				}
+			}
+			break;
+
+		case 2:
+			System.out.println("Enter city or state to search a person");
+			String search = sc.next();
+			aBookManager.searchPersonInBook(search);
+			break;
+		case 3:showContactGroupedByCity();
+			break;
+		case 4:showContactGroupedByState();
+			break;
+		case 5:
+			System.exit(0);
+			break;
+			}
+		}
+	}
 	}
 }
